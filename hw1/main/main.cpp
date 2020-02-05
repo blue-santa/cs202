ea618b8e5c3eb2c5e7387376e9c0006c6ce35aac
Siddhartha <siddhartha-crypto@protonmail.com>
2020-02-05 14:50:22 -0900

    time works in time-it hw1

--- hw1/main/main.cpp
+++ hw1/main/main.cpp
@@ -27,9 +27,9 @@ using std::round;
 class StopWatch {
 
     private:
-        std::time_t _start_time = (time_t)(-1);
-        std::time_t _finish_time = (time_t)(-1);
-        int iter = 0;
+        std::chrono::high_resolution_clock::time_point _start_time;
+        std::chrono::high_resolution_clock::time_point _finish_time;
+        std::chrono::duration<double> _dur;
 
     public:
 
@@ -38,28 +38,20 @@ class StopWatch {
         };
 
         void startClock() {
-            auto s = std::chrono::high_resolution_clock::now(); 
-            _start_time = std::chrono::high_resolution_clock::to_time_t(s);
+            _start_time = std::chrono::high_resolution_clock::now(); 
         }
         
         void captureFinishTime() {
-            auto f = std::chrono::high_resolution_clock::now(); 
-            _finish_time = std::chrono::high_resolution_clock::to_time_t(f);
-            this->reportRawTimes();
+            _finish_time = std::chrono::high_resolution_clock::now(); 
         }
 
         double reportFinishTime() {
-            this->reportRawTimes();
-            double _val = difftime(_finish_time, _start_time);
-            cout << "difftime: " << _val << endl;
-            this->reportRawTimes();
-            return _val;
+            _dur = std::chrono::duration_cast<std::chrono::duration<double>>(_finish_time - _start_time);
+            return (double)(_dur.count());
         } 
 
         void reportRawTimes() { 
-            cout << "_start_time: " << _start_time << " _finish_time: " << _finish_time << endl;
-            ++iter;
-            cout << "iter: " << iter << endl;
+            cout << "_start_time: " << _start_time.time_since_epoch().count() << " _finish_time: " << _finish_time.time_since_epoch().count() << endl;
         }
 };
 
@@ -83,7 +75,7 @@ vector<StopWatch> calcTime(const vector<int> calcObj, const vector<int> searchOb
     vector<StopWatch> tests;
 
     // Declare moment testing
-    cout << "Initiating Search method test" << endl;
+    cout << "Initiating Search method test\nSearchObj: " << searchObj[0] << endl;
 
     // Search method tests 
     for (int i = 0; i < 5; i++) {
@@ -93,10 +85,22 @@ vector<StopWatch> calcTime(const vector<int> calcObj, const vector<int> searchOb
         it = std::search(calcObj.begin(), calcObj.end(), searchObj.begin(), searchObj.end()); 
         test.captureFinishTime(); 
         tests.push_back(test); 
-        cout << "Completed Search method test (" << i << ") with a final time of: " << test.reportFinishTime() << endl;
-        test.reportRawTimes();
+        cout << "Completed Search method test (" << i + 1 << ") with a final time of: " << test.reportFinishTime() << endl << endl;;
     }
 
+    // Declare moment testing
+    cout << "Binary Search method test" << endl;
+
+    // Search method tests 
+    for (int i = 0; i < 5; i++) {
+        // Declare and initiate StopWatch variables for search method
+        vector<int>::const_iterator it; 
+        StopWatch test; 
+        it = std::search(calcObj.begin(), calcObj.end(), searchObj.begin(), searchObj.end()); 
+        test.captureFinishTime(); 
+        tests.push_back(test); 
+        cout << "Completed Search method test (" << i + 1 << ") with a final time of: " << test.reportFinishTime() << endl << endl;;
+    }
     return tests; 
 }
 
