58fc2438d271e272f36f987cc0de9074dee7a537
Siddhartha <siddhartha-crypto@protonmail.com>
2020-02-10 22:08:12 -0900

    lab02-supplemental initiate

---
+++ lab02-supplemental/header.hpp
@@ -0,0 +1,56 @@
+/*
+ * header.hpp
+ * CS 202
+ * February 3, 2019
+ * Bryan Beus
+ * Declaration file
+ */
+
+#ifndef HEAD_HPP
+#define HEAD_HPP 
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+using std::istringstream;
+using std::setw;
+using std::left;
+
+class Vector3f {
+
+    private:
+        int _x, _y;
+
+    public:
+        MyClass();
+        MyClass(const MyClass &origPtr);
+        MyClass(const int &subX, const int &subY); 
+
+        ~MyClass();
+
+        int getX() const;
+        int getY() const;
+
+        void setX(int newX);
+        void setY(int newY);
+
+};
+
+void passByValue(MyClass obj);
+void passByReference(MyClass &obj);
+void passByRefToConst(const MyClass &obj);
+
+MyClass returnNewObj();
+
+vector<MyClass> myVec();
+
+#endif
+
---
+++ lab02-supplemental/source_file.cpp
@@ -0,0 +1,94 @@
+/*
+ * source_file.cpp
+ * CS 202
+ * February 3, 2019
+ * Bryan Beus
+ * Definition file for MyClass
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+#include "header.hpp"
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+using std::istringstream;
+using std::setw;
+using std::left;
+
+MyClass::MyClass() {
+    _x = 1;
+    _y = 2;
+}
+
+MyClass::MyClass(const MyClass &origPtr) {
+    _x = origPtr._x;
+    _y = origPtr._y;
+}
+
+MyClass::MyClass(const int &subX, const int &subY): 
+    _x(subX), 
+    _y(subY) 
+{}
+
+int MyClass::getX() const {
+    return _x;
+}
+
+int MyClass::getY() const {
+    return _y;
+}
+
+void MyClass::setX(int newX) {
+    _x = newX;
+}
+
+void MyClass::setY(int newY) {
+    _y = newY;
+}
+
+MyClass::~MyClass() {};
+
+void passByValue(MyClass obj) {
+
+    cout << "passByValue: " <<  obj.getX() << " " << obj.getY() << endl;
+}
+
+void passByReference(MyClass &obj) {
+
+    obj.setX(42);
+    obj.setY(53);
+
+    cout << "passByReference: " <<  obj.getX() << " " << obj.getY() << endl;
+
+}
+
+void passByRefToConst(const MyClass &obj) {
+
+    cout << "passByValue: " <<  obj.getX() << " " << obj.getY() << endl; 
+
+}
+
+MyClass returnNewObj() { 
+    MyClass obj;
+    return obj; 
+}
+
+vector<MyClass> myVec() {
+
+    MyClass obj;
+
+    vector<MyClass> myVec (5, obj);
+
+    return myVec;
+
+}
+
+
