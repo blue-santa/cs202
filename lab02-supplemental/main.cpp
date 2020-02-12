88ad806a0f0c3e5cc8b84600fa5268cb6886aea0
siddhartha <siddhartha-crypto@protonmail.com>
2020-02-11 21:47:24 -0900

    initiate tests in catch

--- lab02-supplemental/main.cpp
+++ lab02-supplemental/main.cpp
@@ -14,6 +14,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CATCH_CONFIG_MAIN
+
 #include "Catch.hpp"
 #include "header.hpp"
 
@@ -27,11 +29,26 @@ using std::size_t;
 using std::stod;
 using std::cerr;
 
-int main(int argc, char** argv) { 
-
-
-
-    return 0;
+unsigned int Factorial(unsigned int number) {
+    return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
+Vector3f vecA;
+Vector3f vecB(1, 2, 3);
+
+TEST_CASE( "Vector3f constructs properly", "[vector]" ) {
+    REQUIRE( vecA.x == 0 );
+    REQUIRE( vecA.y == 0 );
+    REQUIRE( vecA.z == 0 );
+    REQUIRE( vecB.x == 1 );
+    REQUIRE( vecB.y == 2 );
+    REQUIRE( vecB.z == 3 );
+}
 
