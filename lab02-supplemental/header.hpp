2728cafbb40b60b6819dc9fa0d5d7df8a53a62e3
siddhartha <siddhartha-crypto@protonmail.com>
2020-02-11 21:12:58 -0900

    develop supplemental lab

--- lab02-supplemental/header.hpp
+++ lab02-supplemental/header.hpp
@@ -25,66 +25,35 @@ using std::istringstream;
 using std::setw;
 using std::left;
 
-class Vector3f {
-
+class Vector3f { 
     public:
-        int x, y, z;
+        float x, y, z;
 
-        Vector3f() {};
-        Vector3f(const int userX, const int userY, const int userZ):
-            x(userX), y(userY), z(userZ) {};
+        Vector3f();
+        Vector3f(const float userX, const float userY, const float userZ);
         Vector3f(const Vector3f& originalVector);
 
-        Vector3f add(const Vector3f& b) {
-            Vector3f newVec(x + b.x, y + b.y, z + b.z);
-            return newVec;
-        }
+        Vector3f add(const Vector3f& b);
 
-        Vector3f sub(const Vector3f& b) {
-            Vector3f newVec(x - b.x, y - b.zy, z - b.z);
-            return newVec;
-        }
+        Vector3f sub(const Vector3f& b);
 
-        // Scale times %
-        float scale(const Vector3f& c) {
-            float fl;
-            return fl;
-        }
+        Vector3f scale(const float c);
 
-        Vector3f negate() {
-            Vector3f newVec(-x, -y -z);
-            return newVec;
-        }
+        Vector3f negate();
 
-        int dot(const Vector3f& b) {
-            int res(x * b.x + y * b.y + z * b.z);
-            return res;
-        }
+        float dot(const Vector3f& b);
  
-        Vector3f cross(const Vector3f& b) {
-            Vector3f res(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
-            return res;
-        }
+        Vector3f cross(const Vector3f& b);
 
-        double length() {
-            double res = sqrt(x * x + y * y + z * z);
-        }
+        double length();
 
-        void unit() {
-            this.scale(1.0f / this.length());
-        }
+        Vector3f unit();
 
 };
 
-bool operator==(const Vector3f& a, const Vector3f& b) {
-    return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
-}
+bool operator==(const Vector3f& a, const Vector3f& b);
 
-bool operator!=(const Vector3f& a, const Vector3f& b) {
-    return (a.x != b.x) && (a.y != b.y) && (a.z != b.z);
-}
-
-
+bool operator!=(const Vector3f& a, const Vector3f& b);
 
 #endif
 
--- lab02-supplemental/source_file.cpp
+++ lab02-supplemental/source_file.cpp
@@ -24,4 +24,61 @@ using std::istringstream;
 using std::setw;
 using std::left;
 
+Vector3f::Vector3f(): 
+    x(0), y(0), z(0) {}; 
+
+Vector3f::Vector3f(const float userX, const float userY, const float userZ): 
+    x(userX), y(userY), z(userZ) {}; 
+
+Vector3f::Vector3f(const Vector3f& originalVector): 
+    x(originalVector.x), y(originalVector.y), z(originalVector.z) {}; 
+
+Vector3f Vector3f::add(const Vector3f& b) { 
+    Vector3f newVec(x + b.x, y + b.y, z + b.z); 
+    return newVec; 
+} 
+
+Vector3f Vector3f::sub(const Vector3f& b) { 
+    Vector3f newVec(x - b.x, y - b.y, z - b.z); 
+    return newVec; 
+} 
+
+Vector3f Vector3f::scale(const float c) { 
+    Vector3f newVec(x * c, y * c, z * c); 
+    return newVec; 
+} 
+
+Vector3f Vector3f::negate() { 
+    Vector3f newVec(-x, -y, -z); 
+    return newVec; 
+} 
+
+float Vector3f::dot(const Vector3f& b) { 
+    float res(x * b.x + y * b.y + z * b.z); 
+    return res; 
+} 
+
+Vector3f Vector3f::cross(const Vector3f& b) { 
+    Vector3f newVec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); 
+    return newVec; 
+} 
+
+double Vector3f::length() { 
+    double res = sqrt(x * x + y * y + z * z); 
+    return res; 
+} 
+
+Vector3f Vector3f::unit() { 
+    Vector3f newVec(this->scale(1.0f / this->length())); 
+    return newVec; 
+} 
+
+bool operator==(const Vector3f& a, const Vector3f& b) { 
+    return (a.x == b.x) && (a.y == b.y) && (a.z == b.z); 
+} 
+
+bool operator!=(const Vector3f& a, const Vector3f& b) { 
+    return (a.x != b.x) && (a.y != b.y) && (a.z != b.z); 
+} 
+
 
