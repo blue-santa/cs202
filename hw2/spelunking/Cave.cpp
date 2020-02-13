15b09d96456b3ef74aac6e108e9c08abe30aea4a
siddhartha <siddhartha-crypto@protonmail.com>
2020-02-12 22:42:08 -0900

    reset Cave.cpp file

--- hw2/spelunking/Cave.cpp
+++ hw2/spelunking/Cave.cpp
@@ -3,31 +3,63 @@
  * CS 202
  * February 11, 2020
  * Bryan Beus
- * Cave member definition file for main in hw2
+ * Definition file for Cave class for spelunking in hw2
  */
 
-#include <iostream>
-#include <iomanip>
 #include <string>
-#include <cstdlib>
-#include <stdlib.h>
-#include <vector>
-#include <sstream>
 #include <fstream>
-#include <iterator>
+#include <stdlib.h>
 
-#include "Miscellaneous.hpp"
-#include "Cave.hpp" 
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::vector;
-using std::getline;
 using std::string;
-using std::endl;
-using std::ifstream;
-using std::stringstream;
-using std::istream_iterator;
-using std::right;
+
+class Cave {
+
+    public:
+        // Return number of rooms in the cave
+        bool size() const;
+
+        // Return current room
+        int getCurrentRoom();
+
+        // Go to room in the cave complex
+        void gotoRoom(int room);
+
+        // Which adjacent room? 0, 1, or 2?
+        void gotoAdjacentRoom(int room);
+
+        // Connect two rooms together
+        void connect(int room1, int room2);
+
+        // Print the short description of the room
+        void printShortDescription(int room) const;
+
+        //Print the long description of the room
+        void printLongDesc(int room) const;
+
+        // Save rooms to an output stream
+        void saveRooms(std::ostream& os) const;
+
+        // Read rooms from an input stream
+        void readRooms(std::istream& is);
+
+    private:
+        struct CaveNode {
+            std::weak_ptr<CaveNode> rooms[MaxAdjacentRooms];
+            std::string shortdesc; // A short description of this room
+            std::string longdesc; // A long description of this room
+
+            // Constructor should initialize rooms to nullptr
+            CaveNode();
+        };
+
+        using CaveNodePtr = std::shared_ptr<CaveNode>;
+
+        static constexpr int MaxAdjacentRooms = 3;
+        std::vector<CaveNodePtr> caveRooms;
+        int currentRoom;
+
+
+};
+
+#endif
 
