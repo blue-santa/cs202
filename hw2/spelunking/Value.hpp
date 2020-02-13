3bbabeaad95480798e56b4e893ba679957dcc2d4
siddhartha <siddhartha-crypto@protonmail.com>
2020-02-12 22:06:25 -0900

    initiate cave project in hw2

---
+++ hw2/spelunking/Value.hpp
@@ -0,0 +1,35 @@
+/*
+ * Value.hpp
+ * CS 202
+ * February 11, 2020
+ * Bryan Beus
+ * Header file for Value class for main in hw2
+ */
+
+#ifndef __VALUE_HPP_
+#define __VALUE_HPP_
+
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+
+using std::string;
+
+struct Value {
+
+    string title = "";
+    int line_count = 0;
+    int par_count = 0;
+    int word_count = 0;
+    int char_count = 0;
+    static int total_change_count;
+
+    Value(string& filename);
+    void setTitle(string& filename);
+    unsigned int countWords(string const& str);
+    void setCount(string& filename);
+    void reportValue();
+};
+
+#endif
+
---
+++ hw2/spelunking/main.cpp
@@ -0,0 +1,108 @@
+/*
+ * main.cpp
+ * CS 202
+ * January 19, 2020
+ * Bryan Beus
+ * Main file for temperature main project in hw0
+ */
+
+// #define CATCH_CONFIG_MAIN
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <list>
+#include <iterator>
+
+#include "Miscellaneous.hpp"
+#include "Value.hpp" 
+#include "Catch.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+using std::list;
+using std::right;
+
+void reportValues(list<Value>& mylist) {
+
+    list<Value>::iterator it = mylist.begin();
+
+    for (unsigned int i = 0; i < mylist.size(); i++) {
+        it->reportValue();
+        it++;
+    }
+
+}
+
+int main(int argc, char* argv[])
+{
+
+    vector<string> filenames;
+    filenames.push_back("the-wanderer.txt");
+    filenames.push_back("dream-of-the-rood.txt");
+    filenames.push_back("solomon-and-saturn.txt");
+    filenames.push_back("the-phoenix.txt");
+    filenames.push_back("wulf-and-eadwacer.txt");
+    filenames.push_back("the-menologium.txt");
+    filenames.push_back("the-order-of-the-world.txt");
+    filenames.push_back("the-fate-of-the-apostles.txt"); 
+
+    list<Value> mylist;
+
+    cout << endl;
+    cout << setw(35 + 12 * 4) << right << "Push three values to the front of the list" << endl;
+    for (unsigned int i = 0; i < 3; i++) {
+        Value newValue(filenames[i]);
+        mylist.push_front(newValue);
+    } 
+    reportValues(mylist);
+    cout << endl;
+
+
+    cout << setw(35 + 12 * 4) << right <<  "Pop a value from the front of the list" << endl;
+    mylist.pop_front(); 
+    reportValues(mylist);
+    cout << endl;
+
+    cout << setw(35 + 12 * 4) << right <<  "Push three values to the back of the list" << endl;
+    for (unsigned int i = 2; i < 5; i++) {
+        Value newValue(filenames[i]);
+        mylist.push_back(newValue);
+    } 
+    reportValues(mylist);
+    cout << endl;
+
+
+    cout << setw(35 + 12 * 4) << right <<  "Pop a value from the back of the list" << endl;
+    mylist.pop_back(); 
+    reportValues(mylist);
+    cout << endl;
+
+    list<Value>::iterator it = mylist.begin();
+
+    cout << setw(35 + 12 * 4) << right <<  "Search for Solomon and Saturn" << endl;
+
+    while (it->title != "solomon-and-saturn.txt") it++;
+
+    it->reportValue();
+
+    cout << endl;
+    cout << setw(35 + 12 * 4) << right <<  "Insert sorted values behind Solomon and Saturn" << endl;
+    for (int i = 0; mylist.size() < filenames.size(); i++) {
+        Value newValue(filenames[(int)(mylist.size())]);
+        mylist.insert(it, newValue);
+        it++;
+    }
+    reportValues(mylist);
+
+    cout << endl;
+    cout << "Total number of file changes: " << Value::total_change_count << endl;
+
+	return 0;
+}
+
+
