c836d2f991f61cc4ac8a4e8e6e75461912db2404
siddhartha <siddhartha-crypto@protonmail.com>
2020-02-24 20:13:41 -0900

    develop supp lab 06

--- lab06-supp/LabGLFW.cpp
+++ lab06-supp/LabGLFW.cpp
@@ -29,11 +29,48 @@ int not_main(void) {
         // Clear color
         glClearColor(0.5, 0.4, 0.3, 1);
 
+        int w=1;
+        int h=1;
+        glfwGetWindowSize(window, &w, &h);
+        glViewport(0, 0, w, h);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        double ar = (double)w/(double)h;
+        glOrtho(-ar, ar, -1, 1, -1, 1);
+        glMatrixMode(GL_MODELVIEW);
+
+        // Begin Triangle Transformation Functions
+        glLoadIdentity(); 
+
+        // Set the angle according to the time in seconds
+        float angle = (float)glfwGetTime(); // This GLFW function returns the time in seconds as a double
+
+        // Declare two floats to determine (x and y? positions)
+        float c = 0.1f * std::cos(angle*3.14159f/180.0f);
+        float s = 0.1f * std::cos(angle*3.14159f/180.0f);
+
+        // Move the x/y positions of the gl object on the screen, according to the calculations
+        glTranslatef(c, s, 0.0f);
+
+        // Scale it as necessary
+        glScalef(0.5 + c, 0.5 + s, 1.0f);
+        glRotatef(angle * 30.0f, 0.0f, 0.0f, 1.0f);
+
         // Draw a triangle
         glBegin(GL_TRIANGLES);
-        glVertex3f(-1.0f, -1.0f, 0.0f); // bottom left
-        glVertex3f(1.0f, -1.0f, 0.0f); // bottom right
-        glVertex3f(0.0f, 1.0f, 0.0f); // middle top
+            glVertex3f(-1.0f, -1.0f, 0.0f); // bottom left
+            glVertex3f(1.0f, -1.0f, 0.0f); // bottom right
+            glVertex3f(0.0f, 1.0f, 0.0f); // middle top
+        glEnd();
+
+        glLoadIdentity();
+        glBegin(GL_LINE_LOOP);
+            for (float a = 0.0f; a < 360.0f; a += 1.0f) {
+                float c = std::cos(a * 3.14159f / 180.0f);
+                float s = std::sin(a * 3.14159f / 180.0f);
+                glColor3f(std::abs(c), std::abs(s), 0.0f);
+                glVertex3f(c, s, 0.0f);
+            }
         glEnd();
 
         /* Swap front and back buffers */
