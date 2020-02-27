5c2d5beac5d0c91131ecf4e22137f10ee7efdf0b
siddhartha <siddhartha-crypto@protonmail.com>
2020-02-27 11:08:40 -0900

    initiate static lib lab

---
+++ lab06-static-lib/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * main.cpp
+ * CS 202
+ * February 27, 2020
+ * Bryan Beus
+ * Main file
+ */
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::vector;
+
+int main(int argc, char** argv) { 
+
+    return 0;
+}
+
+
---
+++ lab06-static-lib/source_file.cpp
@@ -0,0 +1,25 @@
+/*
+ * source_file.cpp
+ * CS 202
+ * February 3, 2019
+ * Bryan Beus
+ * Definition file for MyClass
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+#include "header.hpp"
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+using std::istringstream;
+using std::setw;
+using std::left;
+
