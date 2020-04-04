18c9539acb63c13d24606f11de3e0ccd7db47eb6
Siddhartha <siddhartha-crypto@protonmail.com>
2020-04-03 22:59:05 -0800

    Lab10: Put runtime_error in functionC

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-// [ ] Put runtime_error in functionC
+using std::cout;
+using std::endl;
+
+// [x] Put runtime_error in functionC
 // [ ] Write object with constructor/destructor message
 // [ ] Write try/catch block
 
@@ -19,32 +22,36 @@ class MyObj {
 void functionC()
 {
     try {
-    } catch(std::runtime_error& e) {
+
+    } catch (std::runtime_error& e) {
         cout << "Runtime error: " << e.what() << endl;
     }
 }
 
 void functionB()
 {
-  //your code here
-  std::cout << "Starting functionB()\n";
-  functionC();
-  std::cout << "Ending functionB()\n";
+    //your code here
+    std::cout << "Starting functionB()\n";
+    functionC();
+    std::cout << "Ending functionB()\n";
 }
 
 void functionA()
 {
-  // your code here
-  functionB();
-  // your code here
-  std::cout << "Caught an exception: " << e.what() << std::endl;
-  // your code here
+    try {
+        functionB();
+        cout << "This code should not be reached." << endl;
+    } catch (std::runtime_error& e) {
+        std::cout << "Caught an exception: " << e.what() << std::endl;
+
+    }
+    cout << "This code should be reached." << endl;
 }
 
 int main()
 {
-  std::cout << "Starting main()" << std::endl;
-  functionA();
-  std::cout << "Ended normally." << std::endl;
-  return 0;
+    std::cout << "Starting main()" << std::endl;
+    functionA();
+    std::cout << "Ended normally." << std::endl;
+    return 0;
 }
