8019a6b934c6705d1c75f62317cb0b405536cf87
Siddhartha <siddhartha-crypto@protonmail.com>
2020-04-07 00:57:25 -0800

    Initiate Challenge 1

---
+++ iditarod/anchorage/main.cpp
@@ -0,0 +1,16 @@
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+
+    return 0;
+}
+
