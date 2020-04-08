305f4b8d6e160fcafd09785a731bfe8426cf4040
Siddhartha <siddhartha-crypto@protonmail.com>
2020-04-07 17:38:25 -0800

    Write a non-recursive function that returns the nth number of the Fib sequence.

--- iditarod/anchorage/main.cpp
+++ iditarod/anchorage/main.cpp
@@ -13,10 +13,39 @@ void printCurrentSetup(const int& current_num, const int& current_value) {
     cout << current_num << "th number: " << current_value << endl;
 }
 
+int fibonacci_non_recursive(const int& final_num) {
+    int current_num = 0; 
+    int current_value = 0;
+    vector<int> values;
+
+    if (current_num < final_num) {
+        printCurrentSetup(current_num, current_value);
+        values.push_back(current_value);
+        current_num++;
+        current_value++;
+        values.push_back(current_value);
+    } 
+
+    if (current_num < final_num) {
+        printCurrentSetup(current_num, current_value);
+        current_num++;
+        values.push_back(current_value);
+    } 
+    
+    
+    while (current_num < final_num) {
+        printCurrentSetup(current_num, current_value);
+        current_num += 1;
+        current_value = values.at(current_num - 1) + values.at(current_num - 2);
+        values.push_back(current_value); 
+    } 
+
+    return current_value; 
+}
 
 int fibonacci_recursive(const int& final_num, int& current_num, int& current_value, vector<int>& values) {
 
-    if (current_num == 0) {
+    if (current_num == 0 && current_num < final_num) {
         printCurrentSetup(current_num, current_value);
         values.push_back(current_value);
         current_num++;
@@ -43,9 +72,13 @@ int main() {
     int final_num = 10;
     int current_num = 0;
     int current_value = 0;
-    vector<int> values;
-    number = fibonacci_recursive(final_num, current_num, current_value, values); 
+    vector<int> values_recursive;
+    number = fibonacci_recursive(final_num, current_num, current_value, values_recursive); 
     cout << "Recursive Fibonacci nth number: " << number << endl;
+
+    // Execute recursive fibonacci number finder
+    number = fibonacci_non_recursive(final_num); 
+    cout << "Non-recursive Fibonacci nth number: " << number << endl;
 
     return 0;
 }
